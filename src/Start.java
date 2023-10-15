d348973caeda1e8c94fb5aafbfc318237100d793
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-15 10:22:44 -0400

    added heads and tails variables

--- src/Start.java
+++ src/Start.java
@@ -2,6 +2,8 @@ import javax.swing.JOptionPane;
 public class Start {
 	public static void main(String[] args) {
 		boolean keepGoing = false;
+		int heads = 0;
+		int tails = 0;
 		while(keepGoing)
 		{
 			keepGoing = false;
